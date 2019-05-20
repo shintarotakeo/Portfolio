03af02fadd6af574f2545d37cd1825d5ff58be48
shintarotakeo <46601775+shintarotakeo@users.noreply.github.com>
2019-05-20 09:37:00 -0400

    Create scraping.py

---
+++ scraping.py
@@ -0,0 +1,16 @@
+# Portfolio
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://qiita.com/') 
+data = BeautifulSoup(r.text, 'html.parser')
+data    
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://newspicks.com/') 
+data = BeautifulSoup(r.text, 'html.parser')
+elems = data.find_all("div", class_="title _ellipsis")
+for e in elems:
+ print(e.getText())
